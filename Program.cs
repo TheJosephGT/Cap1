e8fd9dec4fd46149604343cb7c8dd5847331ae1f
JosephGT <joseph2002610@gmail.com>
2023-01-28 22:01:13 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    /*
+    
+    Cree un proyecto nuevo que imprima su
+    nombre en la ventana de la consola.
+
+    Agregue más mensajes a la aplicación
+    que ha creado.
+
+    */
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Mi nombre es Joseph Garcia Torres");
+
+        System.Console.WriteLine("Mas mensajes");
+        System.Console.WriteLine("Agregando");
+    }
+}
